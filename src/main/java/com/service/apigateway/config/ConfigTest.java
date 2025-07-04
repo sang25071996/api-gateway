5154f8a4526676a6171554fa24a2f82c874cbab7
Sang Nguyen <nguyentansang96xuanba@gmail.com>
2025-07-04 20:45:03 +0700

    add new config 2

---
+++ src/main/java/com/service/apigateway/config/ConfigTest.java
@@ -0,0 +1,12 @@
+package com.service.apigateway.config;
+
+public class ConfigTest {
+
+    public String test() {
+        return "This is a test method in ConfigTest class.";
+    }
+
+    public String hello() {
+        return "Hello from ConfigTest!";
+    }
+}
